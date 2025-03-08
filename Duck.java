861bdae132d58e9675ef0ebf720626100c116192
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-08 18:45:12 +0530

    Created abstract class Duck

---
+++ Duck.java
@@ -0,0 +1,7 @@
+abstract public class Duck 
+{
+    // Behavior interfaces for flying, quacking, and swimming
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    SwimBehavior swimBehavior;
+
