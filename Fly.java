440e3af8a4ed35990bb6a441d448f2f27c806745
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-08 18:47:34 +0530

    FlyBehavior for flying

---
+++ Fly.java
@@ -0,0 +1,9 @@
+public class Fly implements FlyBehavior 
+{
+    // override fly method for ducks that fly
+    @Override
+    public void fly() 
+    {
+        System.out.println("I am flying high!");
+    }
+}
