c3f80aaa9a2813d87c76e51590b9b08c3411b8a4
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-08 18:50:38 +0530

    Added MallardDuck methods

--- Main.java
+++ Main.java
@@ -2,3 +2,11 @@ public class Main
 {
     public static void main(String[] args) 
     {
+        // Mallard Duck
+        Duck mallard = new MallardDuck();
+        mallard.display();
+        mallard.performQuack();
+        mallard.performSwim();
+        mallard.performFly();
+        System.out.println("-------------------");
+
